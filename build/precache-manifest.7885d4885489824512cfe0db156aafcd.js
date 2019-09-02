9daf0fcddabe281c6f23e5ff9dc4e7535a3a8d13
Scratchyone <>
2019-09-01 21:07:20 -0400

    fixed build

---
+++ build/precache-manifest.7885d4885489824512cfe0db156aafcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4562dd2b07941b0199fad43dc4db2d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938313d2cfaf1eb27881",
+    "url": "/static/css/main.061eef50.chunk.css"
+  },
+  {
+    "revision": "4076dc5beefc21b0355a",
+    "url": "/static/js/2.c2311c89.chunk.js"
+  },
+  {
+    "revision": "938313d2cfaf1eb27881",
+    "url": "/static/js/main.7c17f27c.chunk.js"
+  },
+  {
+    "revision": "98e0357775670d8b4472",
+    "url": "/static/js/runtime~main.ffef4ee0.js"
+  }
+]);
