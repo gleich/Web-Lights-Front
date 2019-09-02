1f238ec6431f9aaaa04d2c0d60dc4ef9bf9d6e4a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-02 01:41:09 +0000

    Deploy Web-Lights/Web-Lights-Front to github.com/Web-Lights/Web-Lights-Front.git:gh-pages

---
+++ precache-manifest.b34093e0d65eaa2afcdf69a98e16168f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0b2f19445d0ca91e83d2018d61e131",
+    "url": "./index.html"
+  },
+  {
+    "revision": "830a0682a73f58c53aae",
+    "url": "./static/css/main.061eef50.chunk.css"
+  },
+  {
+    "revision": "4076dc5beefc21b0355a",
+    "url": "./static/js/2.c2311c89.chunk.js"
+  },
+  {
+    "revision": "830a0682a73f58c53aae",
+    "url": "./static/js/main.c2719090.chunk.js"
+  },
+  {
+    "revision": "6a6f4e6f1cf187208a79",
+    "url": "./static/js/runtime~main.53352b4c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c3de440db7fe5017a25f090af09902f.js"
+  "./precache-manifest.b34093e0d65eaa2afcdf69a98e16168f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
