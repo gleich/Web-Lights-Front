b9b5488cb971e37e9cb57b82311820793afc10c6
Scratchyone <>
2019-09-01 21:35:54 -0400

    fixed

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -4,7 +4,7 @@ exports.makeUppercase = functions.database
   .ref('/lights/active')
   .onUpdate((snapshot, context) => {
     // Grab the current value of what was written to the Realtime Database.
-    const original = snapshot.val();
+    const original = snapshot;
     let x = [false, false, false];
     x[original - 1] = true;
     functions.database.ref('/lights/list').set(x);
